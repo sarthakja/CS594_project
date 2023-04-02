8e1934407011cbe745c42a3b9e371877ef01cb4b
Gyeongeun Lee <43015201+gyeongeunlee16@users.noreply.github.com>
2023-04-01 19:34:25 -0500

    Add files via upload
    Added GCN

--- src/gcn.py
+++ src/gcn.py
@@ -6,8 +6,8 @@ import numpy as np
 from random import shuffle
 from torch.nn import Linear
 import torch.nn.functional as F
-#from torch_geometric.nn import GCNConv
-#from torch_geometric.nn import global_mean_pool
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import global_mean_pool
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir = os.path.dirname(dir_path)
@@ -64,31 +64,57 @@ for graph in combined:
     adj_matrix = nx.to_numpy_matrix(graph)
     adj_matrices.append(adj_matrix)
 
-#print(adj_matrices)
-
 # getting num features
 for i, j in enumerate(combined[0].nodes.data()):
     if i == 0:
         for node, dict in combined[0].nodes.data():
             num_node_features = len(dict)
-print(num_node_features)
 
 node_feature_matrices = []
 
 for graph in combined:
     num_nodes = graph.number_of_nodes()
-    node_features = np.zeros((num_nodes, num_node_features))
+    node_features = np.zeros((num_nodes, num_node_features), dtype=object)
 
     for i, node in enumerate(graph.nodes()):
         # dictionary of node features
         node_dict = graph.nodes[node]
-        #node_dict.pop('res_name')
-        #node_dict.pop('res_ss')
+        node_dict['res_name'] = np.array(node_dict['res_name'])
+        node_dict['res_ss'] = np.array(node_dict['res_ss'])
 
         for j, (key, val) in enumerate(node_dict.items()):
-            print(j, (key, val))
-            node_features[i, j] = val
+            if isinstance(val, float):
+                node_features[i, j] = np.array([val])
+            else:
+                node_features[i, j] = val
 
     node_feature_matrices.append(node_features)
 
-print(node_feature_matrices)
+print(len(node_feature_matrices))
+print(len(adj_matrices))
+print(num_node_features)
+num_classes = len(set([g.graph['y'] for g in combined]))
+
+# a simple GNN model
+class GCN(torch.nn.Module):
+    def __init__(self, hidden):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(num_node_features, hidden)
+        self.conv2 = GCNConv(hidden, hidden)
+        self.lin = Linear(hidden, num_classes)
+
+    def forward(self, x, edge_index):
+        # 1. Obtain node embeddings
+        x = torch.cat([self.conv1(adj_matrices[i]*node_feature_matrices[i]) for i in range(len(adj_matrices))], dim=0)
+        x = F.relu(x)
+        x = torch.cat([self.conv2(adj_matrices[i]*node_feature_matrices[i]) for i in range(len(adj_matrices))], dim=0)
+
+        # 2. Readout layer
+        x = global_mean_pool(x)
+
+        # 3. Apply a final classifier
+        x = self.lin(x)
+        return x
+
+model = GCN(hidden=32)
+print(model)
